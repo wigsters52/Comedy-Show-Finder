2ed986038988c122b9bd789bb81d57fc185ce7d5
Mitch Webster <mitch83083@gmail.com>
2019-06-22 13:55:16 -0400

    functions for creating elements and appending to table

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,2 +1,26 @@
 // search box value
 // append api information to table
+
+const select = id => document.getElementById(id)
+const createEl = el => document.createElement(el)
+const setEl = (el, text) => {
+  const element = document.createElement(el)
+  element.innerHTML = text
+  return element
+}
+
+const tableEntry = response => {
+  const row = createEl('tr')
+  const venue = setEl('td', 'The Beachland Ballroom')
+  const city = setEl('td', 'Cleveland')
+  const price = setEl('td', '$100')
+  row.append(venue, city, price)
+  select('tbody').append(row)
+}
+
+const videoData = response => {
+  const video = createEl('video')
+  video.setAttribute('src', response.data.url)
+}
+
+tableEntry()
